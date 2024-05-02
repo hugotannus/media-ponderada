9d2f398ca59caea73debeb88737c5d544991795c
Prof. Hugo <hugo.tannus@go.senac.br>
2024-05-02 15:56:21 -0300

    Adiciona instruções de atividades ao projeto

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 Exemplo de como utilizar as linguagens html, css e javascript para criar uma
 aplicação web, sem necessitar do uso de um framework.
 
-## References
+# References
 
 - [JavaScript Guide - Modules](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Modules)
--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const button = document.querySelector("#botao_verificar");
+let button = document.querySelector("#botao_verificar");
+
 button.addEventListener("click", verificar);
 
-
 function verificar() {
-  
+  /* TODO.4: transfira a solução desenvolvida no arquivo html para cá */
 }
