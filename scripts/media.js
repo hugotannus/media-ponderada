5e83464bf76145587aca1c5f2fb109707109dce5
Prof. Hugo <hugo.tannus@go.senac.br>
2024-05-02 14:34:05 -0300

    Prepara o projeto

--- scripts/media.js
+++ scripts/media.js
@@ -3,5 +3,12 @@ function calculaMedia(n1, n2, n3, n4, p1 = 1, p2 = 1, p3 = 1, p4 = 1) {
 }
 
 function verificaAprovacao(media, presenca = 0.75) {
-    return media >= 7 && presenca >= 0.75 ? "Aprovado" : "Reprovado"
+    return media >= 7 && presenca >= 0.75
+}
+
+function verificaSituacao(n1, n2, n3, n4, presenca = 0.75, p1 = 1, p2 = 1, p3 = 1, p4 = 1) {
+    let media = calculaMedia(n1, n2, n3, n4, p1 = 1, p2 = 1, p3 = 1, p4 = 1);
+    let aprovado = verificaAprovacao(media, presenca)
+
+    return { media, aprovado }
 }
